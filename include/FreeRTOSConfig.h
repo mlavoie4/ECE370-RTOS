25cd5e290c1d19692f43f974c5476c2621a6f2d8
mlavoie4 <55217367+mlavoie4@users.noreply.github.com>
2019-10-09 09:37:33 -0400

    Update FreeRTOSConfig.h

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -32,7 +32,7 @@
 #define configCPU_CLOCK_HZ                      16000000
 #define configTICK_RATE_HZ                      250
 #define configMAX_PRIORITIES                    5
-#define configMINIMAL_STACK_SIZE                32
+#define configMINIMAL_STACK_SIZE                64
 #define configMAX_TASK_NAME_LEN                 16
 #define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
